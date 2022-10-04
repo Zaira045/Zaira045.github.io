01c431c46279f480f9b22a82c77a6c2947f37f9a
Zaira Fuentes <zairafuentes@berkeley.edu>
2022-10-04 10:58:40 -0700

    airtable integration

--- src/Sections/projects.js
+++ src/Sections/projects.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ProjComponent from "../Components/projectComponent";
+import axios from "axios";
 export default function ProjectContainer(props) {
+    const [exper, setExper] = React.useState(null);
+    async function fetchData() {
+        const res =  await axios.get(
+            "https://api.airtable.com/v0/app7fAwO3jsxV706F/Table%201?maxRecords=3&view=Grid%20view", 
+            {
+                headers: {
+                    authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+                },
+            }
+        
+            );
+            console.log(res.data);
+            setExper(res.data.records);
+    }
+    React.useEffect(() => {
+        fetchData();
+      }, []);
+    
+    if (!exper) {
+        return <div>Loading...</div>;
+    }
+
     return (
+
         <div>
+    
             <div id='projects'>
-            <ProjComponent imageLink = "https://inst.eecs.berkeley.edu/~cs61b/sp22/materials/proj/proj3/image/split_point.png" title = "Gitlet" content = "Implemented version control system in Java inspired in Git for a Data Structures course.  Supports features including adding and committing files, creating branches, merging branches, and pushing and pulling from remote branches. " ></ProjComponent>
-            <ProjComponent imageLink = "https://inst.eecs.berkeley.edu/~cs61b/sp22/materials/proj/proj1/img/figure3.png" title = "Enigma" content = " Developed a digital representation of WWII German Enigma cipher machine in Java enabling encryption and decryption.  Used object-oriented design, string manipulation, and file I/O to replicate mechanical elements of original machine" ></ProjComponent>
-            <ProjComponent imageLink = "https://th.bing.com/th/id/R.d72db2c623415f186f297d4caf8daf04?rik=mqRrb%2bbc%2bDogmg&riu=http%3a%2f%2fcaseytakeda.com%2fassets%2fprojimages%2fscheme.png&ehk=ZPETFEUkRZQMOiDPo%2f90khkNLAryCK1HDQaf%2bUOaBHc%3d&risl=&pid=ImgRaw&r=0" title = "Scheme" content = "The Scheme project involves writing an interpreter for the Scheme language. In this project, I developed an interpreter for a subset of the Scheme language." ></ProjComponent>
+            {exper.map((record) => (
+          <ProjComponent imageLink={record.fields.Image} title={record.fields.Title} content = {record.fields.Content}></ProjComponent>
+        ))}
             </div>
         </div>
     );
